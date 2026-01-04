e2b8df810c8ebe3d968bb82b1c0f03e133de8106
Emilija <emilija.zuk@gmail.com>
2026-01-04 20:00:10 +1000

    Main target group is removed. Green and blue target groups are added.

--- infra/modules/alb/main.tf
+++ infra/modules/alb/main.tf
@@ -11,8 +11,8 @@ resource "aws_lb" "main" {
   }
 }
 
-resource "aws_lb_target_group" "main" {
-  name        = "${var.project_name}-tg"
+resource "aws_lb_target_group" "blue" {
+  name        = "${var.project_name}-tg-blue"
   port        = var.app_port
   protocol    = "HTTP"
   vpc_id      = var.vpc_id
@@ -24,7 +24,24 @@ resource "aws_lb_target_group" "main" {
   }
 
   tags = {
-    Name = "${var.project_name}-tg"
+    Name = "${var.project_name}-tg-blue"
+  }
+}
+
+resource "aws_lb_target_group" "green" {
+  name        = "${var.project_name}-tg-green"
+  port        = var.app_port
+  protocol    = "HTTP"
+  vpc_id      = var.vpc_id
+  target_type = "ip"
+
+  health_check {
+    protocol = "HTTP"
+    path     = var.health_check_path
+  }
+
+  tags = {
+    Name = "${var.project_name}-tg-green"
   }
 }
 
@@ -35,6 +52,6 @@ resource "aws_lb_listener" "http" {
 
   default_action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.main.arn
+    target_group_arn = aws_lb_target_group.blue.arn
   }
 }
--- infra/modules/alb/outputs.tf
+++ infra/modules/alb/outputs.tf
@@ -2,8 +2,12 @@ output "alb_dns_name" {
   value = aws_lb.main.dns_name
 }
 
-output "target_group_arn" {
-  value = aws_lb_target_group.main.arn
+output "target_group_blue_arn" {
+  value = aws_lb_target_group.blue.arn
+}
+
+output "target_group_green_arn" {
+  value = aws_lb_target_group.green.arn
 }
 
 
